0aaf4ceaeb81a18ffb6978692e9924a17eed642d
DanMannMann <dan@marsman.co.uk>
2016-09-04 21:21:30 +0100

    Re-add test app

---
+++ CatFlap.TestApp/CatFlap Model for edmx3.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using $contexttypenamespace$;
+
+namespace App
+{
+    public class CatFlap_Model_for_edmx3 : Modeler<$contexttype$>
+    {
+		$classbody%
+    }
+}
